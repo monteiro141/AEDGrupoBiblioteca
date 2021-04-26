0601478a34b20614a26662f3bd549c756513d5aa
monteiro141 <monteiro141@hotmail.com>
2021-04-26 17:19:40 +0100

    Livros v1.1

--- include/livrosArvores.h
+++ include/livrosArvores.h
@@ -7,6 +7,7 @@ struct NodoAB {
 
 typedef struct NodoAB *PNodoAB;
 
+/*----------Funções Auxiliares----------*/
 PNodoAB CriarABP(LIVRO L);
 
 PNodoAB CriarNodoAB(LIVRO L);
@@ -15,14 +16,27 @@ int PesquisarABP (PNodoAB t, LIVRO L);
 
 PNodoAB InserirABP (PNodoAB t, LIVRO L);
 
-PNodoAB LibertarNodoAB(PNodoAB P);
-
 int AlturaAB (PNodoAB t);
 
 PNodoAB PesquisarAB (LIVRO L, PNodoAB t);
 
 PNodoAB ProcurarFolhaAB (PNodoAB t, LIVRO *L);
 
-PNodoAB RemoverNodoAB (PNodoAB t);
+PNodoAB LibertarNodoAB(PNodoAB t);
 
-PNodoAB RemoverAB (PNodoAB t, LIVRO L);
+PNodoAB RemoverNodoABP (PNodoAB t);
+
+PNodoAB RemoverABP (PNodoAB t, LIVRO L) ;
+
+/*----------Inserir livro----------*/
+PNodoAB InserirLivro(PNodoAB t, LIVRO L);
+
+/*----------Remover livro----------*/
+PNodoAB RemoverLivro(PNodoAB t, LIVRO L);
+
+/*----------Alterar livro(ISBN)----------*/
+PNodoAB AlterarLivroAux(PNodoAB t, LIVRO L, LIVRO X);
+PNodoAB AlterarLivro(PNodoAB t, LIVRO L,LIVRO X);
+
+/*----------Consultar Livro----------*/
+void ConsultarLivroISBN(PNodoAB t, int ISBN);
--- src/livrosArvores.c
+++ src/livrosArvores.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 #include "livrosArvores.h"
 #include <stdlib.h>
 
+/*----------Funções Auxiliares----------*/
 PNodoAB CriarABP(LIVRO L)
 {
     PNodoAB t = NULL;
@@ -23,6 +25,8 @@ int PesquisarABP(PNodoAB t, LIVRO L)
 {
     if(t==NULL)
         return 0;
+    if(CompararLivros(L,t->Elemento)==0)
+      return 1;
     if(CompararLivros(L,t->Elemento)==-1)
         return PesquisarABP(t->Esquerda,L);
     else
@@ -42,15 +46,6 @@ PNodoAB InserirABP (PNodoAB t, LIVRO L)
     else
         t->Direita = InserirABP(t->Direita,L);
     return t;     
-}
-
-PNodoAB LibertarNodoAB(PNodoAB t)
-{   
-    t->Esquerda = NULL;
-    t->Direita = NULL;
-    free(t);
-    t = NULL;
-    return t;
 }
 
 int AlturaAB (PNodoAB t)
@@ -92,48 +87,113 @@ PNodoAB ProcurarFolhaAB (PNodoAB t, LIVRO *L)
         return ProcurarFolhaAB(t->Direita, L);
 }
 
-PNodoAB RemoverNodoAB (PNodoAB t)
-{
-  PNodoAB P, R;
-  LIVRO L;
-  if (t->Esquerda == NULL && t->Direita == NULL) // T é uma folha
-  {   
+PNodoAB LibertarNodoAB(PNodoAB t){
+  t->Esquerda = NULL;
+  t->Direita = NULL;
+  free(t);
+  t = NULL;
+  return t;
+}
+
+PNodoAB RemoverNodoABP (PNodoAB t){
+  PNodoAB  R;
+  LIVRO  L;
+  if (t->Esquerda == NULL && t->Direita == NULL) {   
     t = LibertarNodoAB(t);
     return t;
   }
-  if (t->Esquerda == NULL) 
-  {   
+  if (t->Esquerda == NULL) {  
     R = t;
     t = t->Direita;
     R = LibertarNodoAB(R);
     return t;
   }
-  if (t->Direita == NULL) 
-  {     
+  if (t->Direita == NULL) {     
     R = t;
     t = t->Esquerda;
     R = LibertarNodoAB(R);
     return t;
   }
-  P = ProcurarFolhaAB(t, &L);
-  P = LibertarNodoAB(P);
+  t->Direita = SubstituirNodoDireita(t->Direita, &L);
   t->Elemento = L;
-  return t;    
+  return t;
 }
 
-PNodoAB RemoverAB (PNodoAB t, LIVRO L)
+PNodoAB RemoverABP (PNodoAB t, LIVRO L) {
+  if (CompararElementos(L, t->Elemento) == 0) {
+    t = RemoverNodoABP(t);
+    return t;
+  }    
+  if (CompararElementos(L, t->Elemento) == -1)
+    t->Esquerda = RemoverABP(t->Esquerda, L);
+  else
+    t->Direita = RemoverABP(t->Direita, L);
+  return t;
+}
+
+/*------------------------------------------------------------*/
+/*----------Inserir livro----------*/
+PNodoAB InserirLivro(PNodoAB t, LIVRO L)
 {
-  PNodoAB P;
-  if (t == NULL)
-    return NULL;
-  if (CompararLivros(t->Elemento, L) == 0) {
-    t = RemoverNodoAB(t);
+  return InserirABP(t,L);
+}
+
+/*----------Remover livro----------*/
+PNodoAB RemoverLivro(PNodoAB t, LIVRO L)
+{
+    return RemoverABP(t,L);
+}
+
+/*----------Alterar livro(ISBN)----------*/
+PNodoAB AlterarLivroAux(PNodoAB t, LIVRO L, LIVRO X)
+{
+    if(t==NULL)
+        return t;
+    if(CompararLivros(L,t->Elemento)==0)
+    {
+      t->Elemento = X;
+      return t;
+    }
+    if(CompararLivros(L,t->Elemento)==-1)
+        return AlterarLivroAux(t->Esquerda,L,X);
+    else
+        return AlterarLivroAux(t->Direita,L,X);  
+}
+
+PNodoAB AlterarLivro(PNodoAB t, LIVRO L,LIVRO X)
+{
+  if(PesquisarABP(t,L)==0)
+  {
+    printf("Livro não existe!\n");
     return t;
   }
-  P = PesquisarAB(L, t->Esquerda);
-  if (P != NULL)
-    t->Esquerda = RemoverAB(t->Esquerda, L);
-  else
-    t->Direita = RemoverAB(t->Direita, L);
-  return t;    
+  return AlterarLivroAux(t,L,X);
 }
+
+/*----------Consultar Livro----------*/
+void ConsultarLivroISBN(PNodoAB t, int ISBN)
+{
+    if(t==NULL)
+        return;
+    if(t->Elemento.ISBN == ISBN)
+    {
+      MostrarLivro(t->Elemento);
+      return;
+    }
+    ConsultarLivroISBN(t->Direita,ISBN);
+    ConsultarLivroISBN(t->Esquerda,ISBN);  
+}
+/*void ConsultarLivroISBN(PNodoAB t, int ISBN)
+{
+    if(t==NULL)
+        return;
+    if(strstr(L,t->Elemento)==0)
+    {
+      t->Elemento = X;
+      return t;
+    }
+    if(CompararLivros(L,t->Elemento)==-1)
+        return PesquisarABP(t->Esquerda,L);
+    else
+        return PesquisarABP(t->Direita,L);      
+}*/
